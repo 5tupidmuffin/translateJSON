75f945e860807155849039100a30362e006af999
5tupidmuffin <rameshwark9500@gmail.com>
2022-09-26 20:25:50 +0530

    made installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="translateJSON",
+    version="1.0",
+    description="A Simple JSON Parser in Python",
+    author="5tupidmuffin",
+    url="https://github.com/5tupidmuffin/translateJSON",
+    package_dir={"": "src"},
+)
